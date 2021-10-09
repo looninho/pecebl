d2d1707e6d9b142926f1783a512899a17a43d493
looninho <nguyen.l@free.fr>
2021-10-09 10:13:36 +0200

    change version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         ],
     },
     install_requires=[
-        'ipython==7.8.0', 'jupyter', 'numba', 'numpy','scipy', 
+        'ipython==7.8.0', 'jupyter', 'numpy','scipy', 
         'sympy', 'pandas', 'pyqtgraph', 'pyopengl', 'matplotlib', 
         'imageio', 'pyculib', 'pycuda', 'scikit-cuda',
     ],
